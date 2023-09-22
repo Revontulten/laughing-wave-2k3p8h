240dcae2a0c798dcfb8bbcaadd4447c294780946
CodeSandbox <support@codesandbox.io>
2023-09-22 15:28:11 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+import Search from "./Search";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App Search</h1>
+
+      <Search />
+    </div>
+  );
+}
